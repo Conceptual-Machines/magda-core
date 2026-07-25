7c07cecf16ee55e86a3ae33221acd4e2e6de4107
Nomad Monad <38782977+lucaromagnoli@users.noreply.github.com>
2026-07-25 07:01:05 +0100

    fix(ui): repaint disabled parameter slots

--- magda/daw/ui/components/chain/params/ParamSlotComponent.hpp
+++ magda/daw/ui/components/chain/params/ParamSlotComponent.hpp
@@ -173,6 +173,7 @@ class ParamSlotComponent : public juce::Component,
 
     void paint(juce::Graphics& g) override;
     void paintOverChildren(juce::Graphics& g) override;
+    void enablementChanged() override;
     void resized() override;
     void lookAndFeelChanged() override;
     void mouseEnter(const juce::MouseEvent& e) override;
